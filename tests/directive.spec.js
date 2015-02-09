fd99bf299495d5cac59e1ac3c45ad9ca6604df2a
srph <seraphipod@gmail.com>
2015-02-10 07:04:53 +0800

    Updated tests.

--- tests/directive.spec.js
+++ tests/directive.spec.js
@@ -1,4 +1,18 @@
 describe('directive', function() {
+  beforeEach(module('app'));
+
+  var element, $timeout, $window, $document, $q;
+  beforeEach(inject(function(_$q_, _$window_, _$document_, _$timeout_) {
+    var $q = _$q_,
+      , $window = _$window_
+      , $document = _$document_
+      , $timeout = _$timeout_;
+  }));
+
+  // YOLO
+  it('should pass', function() {
+    expect(true).toBe(true);
+  });
 
   describe('scroll handler / infinite scroll', function() {
     describe('halt execution', function() {
@@ -7,6 +21,17 @@ describe('directive', function() {
       it('should not scroll when promise is not null');
     });
 
-    it('should trigger when the scroll reaches the bottom + threshold');
+    describe('trigger when the scroll reaches the bottom + threshold', function() {
+      describe('trigger', function() {
+        it('should trigger for window');
+        it('should trigger for parent container');
+        it('should trigger for HTMLElement (where the directive is applied)');
+        it('should trigger for element');
+        it('should trigger for element (string)');
+      });
+
+      it('should execute callback in <throttle-ms> (e.g, 500ms)');
+      it('should assign promise to null only after the callback is finished (testing async)');
+    });
   });
 });
