0007f63b2ccac2dab638e4e5b7b762a88da2f7c2
srph <seraphipod@gmail.com>
2015-02-12 18:52:23 +0800

    Added states.

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 +function(angular, undefined) {
   angular
-    .module('app', ['srph.infinite-scroll'])
+    .module('app', ['ui.router', 'srph.infinite-scroll'])
+    .config(statesBlock)
     .controller('ContainerController', ContainerController)
     .controller('WindowController', WindowController);
+
+  function statesBlock($stateProvider) {
+
+    var example = {
+      name: 'example'
+      url: '',
+      abstract: true
+    };
+
+    var home = {
+      parent: 'example'
+      name: 'home',
+      url: '/'
+    };
+
+    var win = {
+      parent: 'example',
+      name: 'window',
+      url: '/window'
+    };
+
+    var con = {
+      parent: 'example',
+      name: 'container',
+      url: '/container'
+    };
+
+    $stateProvider.state(example);
+    $stateProvider.state(home);
+    $stateProvider.state(win);
+    $stateProvider.state(con);
+  }
 
   function ContainerController($scope) {
 
